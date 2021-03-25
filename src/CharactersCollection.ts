7cd9d1c1faf4a5b9655366d173fa5f049d9d6a03
Chad Hinds <chadrickj8@gmail.com>
2021-03-25 10:56:24 -0400

    add compare and swap functions

--- src/CharactersCollection.ts
+++ src/CharactersCollection.ts
@@ -5,7 +5,19 @@ export class CharactersCollection {
         return this.data.length
     }
 
-    compare(leftIndex: number, rightIndex: number) {
+    compare(leftIndex: number, rightIndex: number): boolean {
         // TODO: write logic for compare function
+        return this.data[leftIndex].toLocaleLowerCase() > this.data[rightIndex].toLocaleLowerCase()
+    }
+    // Swap characters in a string
+    swap(leftIndex: number, rightIndex: number): void {
+        const characters = this.data.split('');
+
+        const leftHand = characters[leftIndex];
+        characters[leftIndex] = characters[rightIndex];
+        characters[rightIndex] = characters[leftIndex];
+
+        this.data = characters.join('')
+
     }
 }
